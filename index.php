557e227c86397cbc8f461e0429277f3fc0e528bb
Paul Trott <paulhtrott@gmail.com>
2016-06-04 19:32:24 -0500

    added favicon and fixed spelling errors and added _blank so links open in new tab

--- index.php
+++ index.php
@@ -29,9 +29,9 @@
 					<div class="col-md-8">
 						<h1>About me</h1>
 						<p>I am a happily married, father of 3. I have a passion for computers, gaming, technology and fishing. 
-						I am a Developer at Group Health Cooperative of Eau Claire in Altoona, Wisconsin. Work predominantly in C# and MVC, 
+						I am a Developer at Group Health Cooperative of Eau Claire in Altoona, Wisconsin. I work predominantly in C# and MVC, 
 						but also do Java and anything else that gets the job done.</p>
-						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott">Learn More</a>
+						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Learn More</a>
 					</div>
 					<div class="col-md-4">
 						<img class="img-responsive img-border" src="Images/profile-pic.jpg" title="Paul H. Trott" alt="Paul H. Trott" />
@@ -44,11 +44,11 @@
 			<div class="row rotate right">
 				<div class="rotate-row">
 					<div class="col-md-12">
-						<h1>My Experiences</h1>
-						<p>My personal projects include <a href="http://www.fishingtackletracker.com/" title="Fishing tackle tracker website">FishingTackleTracker.com</a> (live, but a work in progress -- <a href="https://bitbucket.org/ptrott/fishingtackletracker.git" title="Fishing tackle tracker code on BitBucket">code</a>) built with Ruby on Rails, 
+						<h1>My Experience</h1>
+						<p>My personal projects include <a href="http://www.fishingtackletracker.com/" title="Fishing tackle tracker website" target="_blank">FishingTackleTracker.com</a> (live, but a work in progress -- <a href="https://bitbucket.org/ptrott/fishingtackletracker.git" title="Fishing tackle tracker code on BitBucket" target="_blank">code</a>) built with Ruby on Rails, 
 						using Git, BitBucket and Heroku, and Papa's Math Game (work in progress - 
-						<a href="https://bitbucket.org/ptrott/numbergenerator.git" title="Number generator dll code on BitBucket">Number Generator</a> and <a href="https://bitbucket.org/ptrott/numbercalculator.git" title="Number calculator dll code on BitBucket">Number Calculator</a>) Math game with the server side portion 
-						written in C#. (<a href="https://docs.google.com/document/d/1NN9IuBH4c6t4Jh6-or70Y6MeGT4A8vfmhdGvbjF1Z-8/edit?usp=sharing" title="Pdf Printable Resume">Printable Resume</a>)</p>
+						<a href="https://bitbucket.org/ptrott/numbergenerator.git" title="Number generator dll code on BitBucket" target="_blank">Number Generator</a> and <a href="https://bitbucket.org/ptrott/numbercalculator.git" title="Number calculator dll code on BitBucket" target="_blank">Number Calculator</a>) Math game with the server side portion 
+						written in C#. (<a href="https://docs.google.com/document/d/1NN9IuBH4c6t4Jh6-or70Y6MeGT4A8vfmhdGvbjF1Z-8/edit?usp=sharing" title="Pdf Printable Resume" target="_blank">Printable Resume</a>)</p>
 					</div>
 					<div class="col-md-6">
 						<img class="img-responsive" src="Images/ghc-logo.png" alt="Group Health Cooperative of Eau Claire Logo" />
@@ -56,7 +56,7 @@
 						<small>Group Health Cooperative of Eau Claire</small>
 						<p>Client and server side web development using C#, HTML, CSS, SCSS, Javascript and jQuery. Use Git and TFS for source control. 
 						Across our four properties the front-end frameworks...</p>
-						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott">Read More</a>
+						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
 					</div>
 					<div class="col-md-6">
 						<img class="img-responsive" src="Images/jamf-logo.png" alt="JAMF software logo" />
@@ -65,7 +65,7 @@
 						<p>Cross platform developer, working on JAMF Software's Objective-C OS X and C# products. Rewrote the Recon.exe command line utility, 
 						implementing certificate based communication and unit testing on the project. Worked in python (v2.7) on the JAMF Distribution Server (JDS), 
 						adding unit tests on the untested project and implementing the unit tests with CI...</p>
-						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott">Read More</a>
+						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
 					</div>
 					<div class="col-md-6">
 						<img class="img-responsive" src="Images/jamf-logo.png" alt="JAMF software logo" />
@@ -73,7 +73,7 @@
 						<small>JAMF Software</small>
 						<p>Migrated internal OS X frameworks to a stable, in-house CocoaPod infrastructure. Created bug fixes for JAMF Binary and JAMF Admin OS X applications 
 						as part of the Casper Suite 9.0 release. Assisted in developing and furthering JAMF client side automated testing.</p>
-						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott">Read More</a>
+						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
 					</div>
 					<div class="col-md-6">
 						<img class="img-responsive" src="Images/cvtc-logo.png" alt="Chippewa Valley Technical college logo" />
@@ -81,7 +81,7 @@
 						<small>Chippewa Valley Technical College</small>
 						<p>Handled tech support calls, emails and web tickets from students and instructors, which include, password issues, email support, network drives, 
 						Citrix, E360, Lync Client and printer support. Provided software and hardware support for telephones, desktops, and laptops.</p>
-						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott">Read More</a>
+						<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
 					</div>
 				</div>
 			</div>
@@ -93,19 +93,19 @@
 					<div class="col-md-12">
 						<h1>Be Social</h1>
 						<p>I am a member of the Chippewa Valley Developers Group. I try to attend the monthly meetings and the yearly conference. 
-						I also attend ThatConference (Wisconsin Dells) and Twin Cities Code Camp (University of Minnesota). If you see me at any meetings, please say hi.</p>
+						I also attend That Conference (Wisconsin Dells) and Twin Cities Code Camp (University of Minnesota). If you see me at any meetings, please say hi.</p>
 					</div>
 					<div class="col-md-3">
-						<a href="https://bitbucket.org/ptrott/"><img src="Images/bitbucket.png" alt="bitbucket repo"></a>
+						<a href="https://bitbucket.org/ptrott/" target="_blank"><img src="Images/bitbucket.png" alt="bitbucket repo"></a>
 					</div>
 					<div class="col-md-3">
-						<a href="https://github.com/paulhtrott"><img src="Images/github.png" alt="github repo"></a>
+						<a href="https://github.com/paulhtrott" target="_blank"><img src="Images/github.png" alt="github repo"></a>
 					</div>
 					<div class="col-md-3">
-						<a href="https://twitter.com/paulhtrott"><img src="Images/twitter.png" alt="twitter"></a>
+						<a href="https://twitter.com/paulhtrott" target="_blank"><img src="Images/twitter.png" alt="twitter"></a>
 					</div>
 					<div class="col-md-3">
-						<a href="https://www.youtube.com/c/paultrott78"><img src="Images/youtube.png" alt="youtube"></a>
+						<a href="https://www.youtube.com/c/paultrott78" target="_blank"><img src="Images/youtube.png" alt="youtube"></a>
 					</div>
 				</div>
 			</div>
