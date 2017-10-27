3da33e94b24fb00808c581deff3644520921eb73
Paul Trott <paultrott78@gmail.com>
2017-10-27 12:17:50 -0500

    added blaze logo, updated page content

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 	<head>
 		<title>Paul H. Trott | Software Developer</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		
-		<!--<link rel="stylesheet" href="Resources/bootstrap-3.3.6-dist/css/bootstrap.min.css" type="text/css">-->
+
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<link rel="stylesheet" href="Styles/paulhtrott.css" type="text/css" media="all">
 	</head>
-	
+
 	<body>
 		<div id="main">
 			<div id="hero">
@@ -17,13 +16,13 @@
 						<h1>What do I do?</h1>
 						<p>I am a <span class="gold">Software Developer</span>...
 						<span>I love to <span class="gold">laugh</span> and have <span class="gold">fun</span> with family and friends.</span>
-						<span>I also love good <span class="gold">food</span>, 
+						<span>I also love good <span class="gold">food</span>,
 						<span class="gold">fishing</span> &amp; <span class="gold">learning</span> new things.</span>
 						</p>
 					</div>
 				</div>
 			</div>
-			
+
 			<div class="container">
 				<div class="row rotate">
 					<div class="rotate-row">
@@ -31,96 +30,106 @@
 							<div class="left"></div>
 						</div>
 					</div>
-					
+
 					<div class="row-content">
 						<div class="col-md-8">
 							<h1>About me</h1>
-							<p>I am a happily married, father of 3. I have a passion for computers, gaming, technology and fishing. 
-							I am a Developer at Group Health Cooperative of Eau Claire in Altoona, Wisconsin. I work predominantly in C# and MVC, 
-							but also do Java and anything else that gets the job done.</p>
-							<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Learn More</a>
+              <p>
+                Married, Father of three. Born in Bermuda, live in Wisconsin. Accountant turned Software Developer. Christian. Fisherman. Occasional hunter. I am a remote Product Developer at TheBlaze based in Irving, Texas. I am a Ruby on Rails developer, working on the premium content products.
+              </p>
+							<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott">Learn More</a>
 						</div>
 						<div class="col-md-4">
 							<img class="img-responsive img-border" src="Images/profile-pic.jpg" title="Paul H. Trott" alt="Paul H. Trott" />
 						</div>
 					</div>
-					
+
 				</div>
-				
+
 				<div class="straight-row">
 					<div class="row right">
 						<div class="col-md-12">
 							<h1>My Experience</h1>
-							<p>My personal projects include <a href="http://www.fishingtackletracker.com/" title="Fishing tackle tracker website" target="_blank">FishingTackleTracker.com</a> (live, but a work in progress -- <a href="https://bitbucket.org/ptrott/fishingtackletracker.git" title="Fishing tackle tracker code on BitBucket" target="_blank">code</a>) built with Ruby on Rails, 
-							using Git, BitBucket and Heroku, and Papa's Math Game (work in progress - 
-							<a href="https://bitbucket.org/ptrott/numbergenerator.git" title="Number generator dll code on BitBucket" target="_blank">Number Generator</a> and <a href="https://bitbucket.org/ptrott/numbercalculator.git" title="Number calculator dll code on BitBucket" target="_blank">Number Calculator</a>) Math game with the server side portion 
-							written in C#. (<a href="https://docs.google.com/document/d/1NN9IuBH4c6t4Jh6-or70Y6MeGT4A8vfmhdGvbjF1Z-8/edit?usp=sharing" title="Pdf Printable Resume" target="_blank">Printable Resume</a>)</p>
+              <p>My personal projects include <a href="http://www.fishingtackletracker.com/" title="Fishing tackle tracker website">FishingTackleTracker.com</a> (live, but seems to always be a work in progress -- <a href="https://github.com/paulhtrott/FishingTackleTracker" title="Fishing tackle tracker code on GitHub">code</a>) built with Ruby on Rails, using Git, BitBucket and Heroku. I also have several other "toys" on GitHub, have a <a href="https://github.com/paulhtrott?tab=repositories" title="My Github Repos">look here</a>.</p>
+<p><a href="https://docs.google.com/document/d/1NN9IuBH4c6t4Jh6-or70Y6MeGT4A8vfmhdGvbjF1Z-8" title="Printable Resume">Printable Resume</a></p>
 						</div>
-						<div class="col-md-6">
-							<img class="img-responsive" src="Images/ghc-logo.png" alt="Group Health Cooperative of Eau Claire Logo" />
-							<h3>IT Software Developer</h3>
-							<small>Group Health Cooperative of Eau Claire</small>
-							<p>Client and server side web development using C#, HTML, CSS, SCSS, Javascript and jQuery. Use Git and TFS for source control. 
-							Across our four properties the front-end frameworks...</p>
-							<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
-						</div>
-						<div class="col-md-6">
-							<img class="img-responsive" src="Images/jamf-logo.png" alt="JAMF software logo" />
-							<h3>Software Engineer - Client Team</h3>
-							<small>JAMF Software</small>
-							<p>Cross platform developer, working on JAMF Software's Objective-C OS X and C# products. Rewrote the Recon.exe command line utility, 
-							implementing certificate based communication and unit testing on the project. Worked in python (v2.7) on the JAMF Distribution Server (JDS), 
-							adding unit tests on the untested project and implementing the unit tests with CI...</p>
-							<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
-						</div>
-						<div class="col-md-6">
-							<img class="img-responsive" src="Images/jamf-logo.png" alt="JAMF software logo" />
-							<h3>Core Team Development Intern</h3>
-							<small>JAMF Software</small>
-							<p>Migrated internal OS X frameworks to a stable, in-house CocoaPod infrastructure. Created bug fixes for JAMF Binary and JAMF Admin OS X applications 
-							as part of the Casper Suite 9.0 release. Assisted in developing and furthering JAMF client side automated testing.</p>
-							<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
-						</div>
-						<div class="col-md-6">
-							<img class="img-responsive" src="Images/cvtc-logo.png" alt="Chippewa Valley Technical college logo" />
-							<h3>Tech Support Agent</h3>
-							<small>Chippewa Valley Technical College</small>
-							<p>Handled tech support calls, emails and web tickets from students and instructors, which include, password issues, email support, network drives, 
-							Citrix, E360, Lync Client and printer support. Provided software and hardware support for telephones, desktops, and laptops.</p>
-							<a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
-						</div>
+            <div class="row">
+              <div class="col-md-6">
+                <img class="img-responsive" src="Images/theblaze-logo.png" alt="TheBlaze Logo" />
+                <h3>Product Developer</h3>
+                <small>TheBlaze</small>
+                <p>Remote Ruby On Rails developer using Ruby, Erb, SCSS, ES6, jQuery, Git, Bitbucket, and Jira. Communication through Slack and Google Hangouts.</p>
+                <a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
+              </div>
+              <div class="col-md-6">
+                <img class="img-responsive" src="Images/ghc-logo.png" alt="Group Health Cooperative of Eau Claire Logo" />
+                <h3>IT Software Developer</h3>
+                <small>Group Health Cooperative of Eau Claire</small>
+                <p>Client and server side web development using C#, HTML, CSS, SCSS, Javascript and jQuery. Use Git and TFS for source control.
+                Across our four properties the front-end frameworks...</p>
+                <a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <img class="img-responsive" src="Images/jamf-logo.png" alt="JAMF software logo" />
+                <h3>Software Engineer - Client Team</h3>
+                <small>JAMF Software</small>
+                <p>Cross platform developer, working on JAMF Software's Objective-C OS X and C# products. Rewrote the Recon.exe command line utility,
+                implementing certificate based communication and unit testing on the project. Worked in python (v2.7) on the JAMF Distribution Server (JDS),
+                adding unit tests on the untested project and implementing the unit tests with CI...</p>
+                <a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
+              </div>
+              <div class="col-md-6">
+                <img class="img-responsive" src="Images/jamf-logo.png" alt="JAMF software logo" />
+                <h3>Core Team Development Intern</h3>
+                <small>JAMF Software</small>
+                <p>Migrated internal OS X frameworks to a stable, in-house CocoaPod infrastructure. Created bug fixes for JAMF Binary and JAMF Admin OS X applications
+                as part of the Casper Suite 9.0 release. Assisted in developing and furthering JAMF client side automated testing.</p>
+                <a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <img class="img-responsive" src="Images/cvtc-logo.png" alt="Chippewa Valley Technical college logo" />
+                <h3>Tech Support Agent</h3>
+                <small>Chippewa Valley Technical College</small>
+                <p>Handled tech support calls, emails and web tickets from students and instructors, which include, password issues, email support, network drives,
+                Citrix, E360, Lync Client and printer support. Provided software and hardware support for telephones, desktops, and laptops.</p>
+                <a class="btn btn-default button" href="http://stackoverflow.com/cv/paulhtrott" target="_blank">Read More</a>
+              </div>
+            </div>
 					</div>
 				</div>
-				
+
 				<div class="row rotate">
 					<div class="rotate-row">
 						<div class="rotate-row-inner">
 							<div class="left"></div>
 						</div>
 					</div>
-					
+
 					<div class="row-content">
 						<div class="col-md-12">
 							<h1>Be Social</h1>
-							<p>I am a member of the Chippewa Valley Developers Group. I try to attend the monthly meetings and the yearly conference. 
-							I also attend That Conference (Wisconsin Dells) and Twin Cities Code Camp (University of Minnesota). If you see me at any meetings, please say hi.</p>
+							<p>I am a member of the Chippewa Valley Developers Group, and attend as the WindyCityRails Conference (Chicago) and Twin Cities Code Camp (Minnesota) whenever I can. If you see me out and about please say hi.</p>
 						</div>
 						<div class="col-md-3">
-							<a href="https://bitbucket.org/ptrott/" target="_blank"><img src="Images/bitbucket.png" alt="bitbucket repo"></a>
+							<a href="https://github.com/paulhtrott"><img src="Images/github.png" alt="github repo"></a>
 						</div>
 						<div class="col-md-3">
-							<a href="https://github.com/paulhtrott" target="_blank"><img src="Images/github.png" alt="github repo"></a>
+							<a href="https://twitter.com/paulhtrott"><img src="Images/twitter.png" alt="twitter"></a>
 						</div>
 						<div class="col-md-3">
-							<a href="https://twitter.com/paulhtrott" target="_blank"><img src="Images/twitter.png" alt="twitter"></a>
+							<a href="https://www.youtube.com/channel/UC-mLlr16GK3Qvas5Lg5VTvA"><img src="Images/youtube.png" alt="youtube"></a>
 						</div>
 						<div class="col-md-3">
-							<a href="https://www.youtube.com/c/paultrott78" target="_blank"><img src="Images/youtube.png" alt="youtube"></a>
+							<a href="https://bitbucket.org/ptrott/"><img src="Images/bitbucket.png" alt="bitbucket repo"></a>
 						</div>
 					</div>
-					
+
 				</div>
-				
+
 				<div class="straight-row">
 					<div class="row right">
 						<div class="col-sm-12">
@@ -130,9 +139,8 @@
 				</div>
 			</div>
 		</div>
-		<script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="Scripts/paulhtrott.js"></script> 
+		<script type="text/javascript" src="Scripts/paulhtrott.js"></script>
 	</body>
-	
 </html>
