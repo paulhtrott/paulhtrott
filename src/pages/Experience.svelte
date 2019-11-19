8e9dfe853f2f44c65de948ed6e17ee94a8a63126
Paul Trott <paulhtrott@gmail.com>
2019-11-18 21:52:26 -0600

    Added images added json file for education and experience data

--- src/pages/Experience.svelte
+++ src/pages/Experience.svelte
@@ -1,11 +1,31 @@
 <script>
   import Title from './../components/Title.svelte'
+
+  import { onMount } from 'svelte'
+
+  let experience = []
+
+  onMount(async () => {
+    fetch("paulhtrott.json")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      experience = data.experience;
+    });
+  });
 </script>
 
 <Title title="Experience" />
 
 <div class="flex flex-row flex-wrap flex-grow mt-2">
   <div class="w-full md:w-1/2 p-3">
-    <h1>Ninja Web Studios</h1>
+    <ul>
+      { #each experience as job }
+        <li>
+          <h1>{job.name}</h1>
+        </li>
+      { /each }
+    </ul>
+    <a href="https://www.linkedin.com/in/paulhtrott">View All Experience</a>
   </div>
 </div>
