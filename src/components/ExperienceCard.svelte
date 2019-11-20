e8b31c0e5df0e588782349fc603d04f96af896e5
Paul Trott <paulhtrott@gmail.com>
2019-11-19 19:18:13 -0600

    Updated gbc and cvtc logos. Added content for experience. Added ExperienceCard component

---
+++ src/components/ExperienceCard.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let skipImage;
+  export let image;
+  export let imageAlt;
+  export let companyName;
+  export let title;
+  export let years;
+  export let description;
+  export let skills;
+</script>
+
+<div class="w-full md:w-1/2 p-3">
+  <div class="rounded overflow-hidden shadow-lg">
+    { #if skipImage !== 'true' }
+      <img class="w-full" src="{image}" alt="{imageAlt}">
+    { /if }
+    <div class="px-6 py-4">
+      <div class="text-sm mb-2">{title}</div>
+      <div class="font-bold text-xl mb-2">{companyName}</div>
+      <div class="text-xs mb-2">{years}</div>
+      <p class="text-gray-700 text-base">
+        {description}
+      </p>
+    </div>
+    <div class="px-6 py-4">
+      { #each skills as skill }
+        <span class="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{skill}</span>
+      { /each }
+    </div>
+  </div>
+</div>
--- src/pages/Experience.svelte
+++ src/pages/Experience.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte'
   import Title from './../components/Title.svelte'
+  import ExperienceCard from './../components/ExperienceCard.svelte'
 
   let experience = []
 
@@ -8,7 +9,6 @@
     fetch("paulhtrott.json")
     .then(response => response.json())
     .then(data => {
-      console.log(data);
       experience = data.experience;
     });
   });
@@ -17,14 +17,9 @@
 <Title title="Experience" />
 
 <div class="flex flex-row flex-wrap flex-grow mt-2">
-  <div class="w-full md:w-1/2 p-3">
-    <ul>
-      { #each experience as job }
-        <li>
-          <h1>{job.name}</h1>
-        </li>
-      { /each }
-    </ul>
-    <a href="https://www.linkedin.com/in/paulhtrott">View All Experience</a>
-  </div>
+    { #each experience as job }
+      <ExperienceCard title="{job.title}" skipImage="true" image="" imageAlt="" description="{job.description}" skills={job.skills} companyName={job.name} years={job.years} />
+    { /each }
 </div>
+
+<a class="float-right mr-5" href="https://www.linkedin.com/in/paulhtrott">View All Experience</a>
